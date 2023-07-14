bca301a205771a1eeb1da7802fc0086f22ac8e8e
pranavi-m <108517788+pranavi-m@users.noreply.github.com>
2023-07-14 19:59:32 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Feed from "../screens/Feed"
+import CreatePost from "../screens/CreatePost"
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+return(
+    <Tab.Navigator
+        screenOptions={({route})=>({
+            tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === 'Feed') {
+                  iconName = focused? 'home' : 'home-outline';
+                }
+                 else if (route.name === 'CreatePost') {
+                  iconName = focused ? 'add-circle' : 'add-circle-outline';
+                }
+                return (
+                <Ionicons 
+                name={iconName} 
+                size={size} 
+                color={color}
+                style={styles.icons} 
+                />
+                )
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: 'black',
+              inactiveTintColor: 'gray',
+            }}
+          >
+            <Tab.Screen name="Feed" component={Feed} />
+            <Tab.Screen name="CreatePost" component={CreatePost} options={{headerShown:false}}/>
+          </Tab.Navigator>
+)
+}
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+      backgroundColor: "#2a2a2a",
+      height: "8%",
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      overflow: "hidden",
+      position: "absolute"
+  },
+  icons: {
+      width: RFValue(30),
+      height: RFValue(30)
+  }
+});
+
+export default BottomTabNavigator
